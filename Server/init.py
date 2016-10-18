178ac634d212d19e0123c5f85e76086c731adc4c
Oliver Hattshire <julian.sepulveda@sansano.usm.cl>
2016-10-18 07:34:00 -0300

    Fixed inclusion errors

--- Server/init.py
+++ Server/init.py
@@ -6,6 +6,17 @@
 ##
 ############
 
-import connector as connector
-import src.catch_yt as yt
-import src.processing as processing
+# Putting "src" to PATH
+import os, sys, inspect
+cmd_folder = os.path.realpath(os.path.abspath(os.path.split(inspect.getfile( inspect.currentframe() ))[0]))
+if cmd_folder not in sys.path:
+    sys.path.insert(0, cmd_folder)
+cmd_subfolder = os.path.realpath(os.path.abspath(os.path.join(os.path.split(inspect.getfile( inspect.currentframe() ))[0],"src")))
+if cmd_subfolder not in sys.path:
+    sys.path.insert(0, cmd_subfolder)
+
+import connector
+import catch_yt as yt
+import processing
+
+print yt.get_info("https://www.youtube.com/watch?v=We11XkhZu7U")
